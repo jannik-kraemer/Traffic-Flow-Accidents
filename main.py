796a27686d8121612c891b3696d0a347afc42185
Jannik Krämer <kraemer.jannik@gmail.com>
2018-02-02 21:51:43 +0100

    Added Dataset import

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Main File
+import numpy as np
+import pandas as pd
+import modules.dataset as ds
+
+dataset = ds.Dataset('data/accidents_2012_to_2014.csv')
+data = dataset.get()
+
+print(dataset.length())
