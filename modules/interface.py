5d12f0c380057fb12367e43144ffcf49d33d1eaf
Jannik Krämer <kraemer.jannik@gmail.com>
2018-02-03 17:27:42 +0100

    Added UI

---
+++ modules/interface.py
@@ -0,0 +1,19 @@
+#
+from appJar import gui
+
+class UI():
+
+    def __init__(self, title = "SimpleText", size="640x480"):
+        # Creates app env.
+        self.app = gui(title, size)
+        # Adds object to widget
+        self.createLabel()
+
+    def createLabel(self, id, text):
+        self.app.addLabel("test", "This is a test")
+
+    def render(self):
+        # Runs everything
+        self.app.go()
+        # Don't put sth down here
+
