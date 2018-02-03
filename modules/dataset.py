185a607cd446add84f1d18602698a971b2113389
Jannik Krämer <kraemer.jannik@gmail.com>
2018-02-03 09:20:07 +0100

    Added export method

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from IPython.display import display, HTML
+
+
 class Dataset:
     def __init__(self, file):
+        self.file = file
         self.data = pd.read_csv(file)
 
     def get(self):
@@ -9,4 +13,12 @@ class Dataset:
     def length(self):
         return len(self.data)
     
-
+    def export_to_html(self, path, rows):
+        print("[Dataset] Creating dataset table... to " + path + "!")
+        self.dataFile = open(path, mode='w')
+        self.dataFile.writelines("<div style='font-family: sans-serif'>")
+        self.dataFile.writelines("<h1> Dataset: <b>"  + self.file + "</b></h1>")
+        self.dataFile.write(self.data.head(n=int(rows)).to_html())
+        self.dataFile.writelines("</div>")
+        self.dataFile.close()
+        print("[Dataset] Export complet!")
