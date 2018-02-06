8670e105a624fa7846d8262d874da64ce4616a6f
Jannik Krämer <kraemer.jannik@gmail.com>
2018-02-06 13:31:39 +0100

    Removed old Django

--- ui/ui/wsgi.py
+++ ui/ui/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ui project.
+WSGI config for UI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ui.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UI.settings")
 
 application = get_wsgi_application()
