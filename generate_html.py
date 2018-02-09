9251349558a77cc758f0731dc10e8230f0437b2d
Jannik Krämer <kraemer.jannik@gmail.com>
2018-02-09 16:09:39 +0100

    Updated core

--- generate_html.py
+++ generate_html.py
@@ -1,6 +1,8 @@
 import modules.dataset as ds
 
-dataset = ds.Dataset('data/accidents_2012_to_2014.csv')
+# dataset = ds.Dataset('data/accidents_2012_to_2014.csv')
+dDataset = input("Enter target: ")
+dataset = ds.Dataset(dDataset)
 data = dataset.get()
 
 dFile = input("Enter file path: ")
