dfd8cf5f96e09120989542fad6b434b4b8918ac4
Jannik Krämer <kraemer.jannik@gmail.com>
2018-02-23 16:04:11 +0100

    Updated UI

--- UI/UI/urls.py
+++ UI/UI/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('core/', include("Interface.urls")),
+    path('interface/', include("Interface.urls")),
     path('admin/', admin.site.urls),
 ]
