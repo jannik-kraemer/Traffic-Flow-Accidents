0cf6432152635df0914bfe34cd0d6d39e02d81b9
Jannik Krämer <kraemer.jannik@gmail.com>
2018-03-02 17:35:39 +0100

    Updated Calculator Interface

--- UI/Algorithm/views.py
+++ UI/Algorithm/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from modules.apiTranslator import Wheater
+from modules import timeStamp
 from django.template import Context, Template
+from modules.algo import calc
+
 
 def view(request):
     t = Template('This is your <span>{{ message }}</span>.')
@@ -10,8 +13,33 @@ def view(request):
 def output(request,user):
     print(user)
 def get(request):
-    lat = request.GET.get('lat', '')
-    lng = request.GET.get('lng', '')
-    print(Wheater.get(lat + "," + lng, "09/02/2018 - 23:01"))
+    lat1 = request.GET.get('lat1', '')
+    lng1 = request.GET.get('lng1', '')
+    lat2 = request.GET.get('lat2', '')
+    lng2 = request.GET.get('lng2', '')
+    date = request.GET.get('date', '')
+    time = request.GET.get('time', '')
+
+    opt1 = {}
+    opt1['lat'] = lat1
+    opt1['lng'] = lng1
+
+    opt2 = {}
+    opt2['lat'] = lat2
+    opt2['lng'] = lng2
+
+    lat, lng = calc.rad(opt1, opt2)
+    wetter = Wheater.Data(str(lat) + "," + str(lng),date + " " + time)
+    print(wetter.get())
+    # print(Wheater.get(lat + "," + lng,date + " " + time))
+    # print(timeStamp.getTimeFromTimestamp())
+
+    sunrise = wetter.translate('daily')['data'][0]['sunriseTime']
+    sunset = wetter.translate('daily')['data'][0]['sunsetTime']
+    targetTimeStamp = timeStamp.getTimestamp(date + " " + time)
+
+    print("Checking values: ")
+    print(Wheater.check(targetTimeStamp, sunrise, sunset))
+
     # c = Context({"lat": lat, "lng": lng})
-    return render(request, "index.html", context={"lat": lat, "lng": lng})
+    return render(request, "index.html", context={"lat": lat, "lng": lng, "lat1": lat1, "lat2": lat2, "lng1": lng1, "lng2": lng2})
