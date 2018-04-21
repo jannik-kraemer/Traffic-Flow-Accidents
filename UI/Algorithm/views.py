49b247f50ce75b992be7d65bdf60652f1bc3bd61
Jannik Krämer <kraemer.jannik@gmail.com>
2018-04-21 19:05:06 +0200

    Updated core

--- UI/Algorithm/views.py
+++ UI/Algorithm/views.py
@@ -3,16 +3,11 @@ from modules.apiTranslator import Wheater
 from modules import timeStamp
 from django.template import Context, Template
 from modules.algo import calc
+from modules.algo import classification
 
-
-def view(request):
-    t = Template('This is your <span>{{ message }}</span>.')
-    c = Context({'message': 'Your message'})
-    html = t.render(c)
-
-def output(request,user):
-    print(user)
 def get(request):
+
+    # Get all args
     lat1 = request.GET.get('lat1', '')
     lng1 = request.GET.get('lng1', '')
     lat2 = request.GET.get('lat2', '')
@@ -21,29 +16,40 @@ def get(request):
     time = request.GET.get('time', '')
 
 
-
+    # Define options
     opt1 = {}
     opt1['lat'] = lat1
     opt1['lng'] = lng1
-
     opt2 = {}
     opt2['lat'] = lat2
     opt2['lng'] = lng2
 
+    # Calculate radius for circle
     lat, lng = calc.rad(opt1, opt2)
-    # print()
+    # Calculate distance
     radius = calc.distance((lat1, lng1), (lat, lng))
-    wetter = Wheater.Data(str(lat) + "," + str(lng),date + " " + time)
-    print(wetter.get())
-    # print(Wheater.get(lat + "," + lng,date + " " + time))
-    # print(timeStamp.getTimeFromTimestamp())
 
+    # Weather
+    # Weather.Data = API Request
+    wetter = Wheater.Data(str(lat) + "," + str(lng),date + " " + time)
+
+    # sunrise & sunset time
     sunrise = wetter.translate('daily')['data'][0]['sunriseTime']
     sunset = wetter.translate('daily')['data'][0]['sunsetTime']
+    
+    # Target timestamps
     targetTimeStamp = timeStamp.getTimestamp(date + " " + time)
 
-    print("Checking values: ")
-    print(Wheater.check(targetTimeStamp, sunrise, sunset))
+    # Debug -> 
+    # print("Checking values: ")
+    # print(Wheater.check(targetTimeStamp, sunrise, sunset))
 
-    # c = Context({"lat": lat, "lng": lng})
-    return render(request, "index.html", context={"lat": lat, "lng": lng, "lat1": lat1, "lat2": lat2, "lng1": lng1, "lng2": lng2, "radius": radius})
+    # Input to the Prediction module
+    dataInput = [[1, int(timeStamp.getDayOfWeek(date)), int(Wheater.check(targetTimeStamp, sunrise, sunset)), 3, 30, 1]]
+
+    # Return Prediction
+    result = classification.prediction(dataInput)
+    print(result)
+
+    # Django VIEW render -> HTML Interface
+    return render(request, "index.html", context={"lat": lat, "lng": lng, "lat1": lat1, "lat2": lat2, "lng1": lng1, "lng2": lng2, "radius": radius, "result": result})
