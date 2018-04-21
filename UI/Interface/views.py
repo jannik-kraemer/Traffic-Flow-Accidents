53c7dce5106a787cafc85dec776188b8617e6f80
Jannik Krämer <kraemer.jannik@gmail.com>
2018-04-21 19:04:36 +0200

    Updates + Doc

--- UI/Interface/views.py
+++ UI/Interface/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, lat=0, lng=5):
-        return render(request, 'index.html', context=None)
+        return render(request, 'index.html', context={"version": "V.1.0"})
